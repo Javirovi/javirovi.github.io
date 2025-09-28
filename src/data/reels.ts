60d20b755b47f9def8d8d683767b19ba3dab7a9f
Javirovi <javirovi22@gmail.com>
2025-09-28 13:38:15 +0200

    Update reels.ts

--- src/data/reels.ts
+++ src/data/reels.ts
@@ -35,7 +35,7 @@ export const reels: ReelsData = {
     },
     {
       id: 'proyecto2',
-      title: 'Lucus Cleanup Reel 2024',
+      title: 'Work In Progress',
       shortDescription: ' ',
       longDescription: 'En 2024 realicé mis prácticas universitarias con el animador gallego David Fidalgo, trabajando durante 4 meses en la producción de su corto "Lucus" como artista de cleanup. También realicé otras tareas, como color e iluminación y en general afiancé muchas de mis aptitudes con Grease Pencil.',
       thumbnail: '/images/placeholder.svg', // For optimized images: import from assets and use .src
@@ -46,7 +46,7 @@ export const reels: ReelsData = {
     },
     {
       id: 'proyecto3',
-      title: 'Cornelius Color Reel 2025',
+      title: 'Work In Progress',
       shortDescription: '',
       longDescription: 'En verano de 2025 tuve la oportunidad de trabajar como artista Ink & Paint en el corto "La Verdad te rechaza, Cornelius.", dirigido por Khris Cembe y Marc Torices mediante Hampa Studio.',
       thumbnail: '/images/placeholder.svg', // For optimized images: import from assets and use .src
@@ -70,7 +70,7 @@ export const reels: ReelsData = {
     },
     {
       id: 'project2',
-      title: 'Lucus Cleanup Reel 2024',
+      title: 'Work In Progress',
       shortDescription: '',
       longDescription: 'Back in 2024 I completed my college internship with galician animator David Fidalgo, working for 4 months on the production of his short film "Lucus" as a cleanup artist. I also performed other tasks such as coloring and lighting. Overall I strenghted a lot of my Grease Pencil skills.',
       thumbnail: '/images/placeholder.svg', // For optimized images: import from assets and use .src
@@ -81,7 +81,7 @@ export const reels: ReelsData = {
     },
     {
       id: 'project3',
-      title: 'Cornelius Color Reel 2025',
+      title: 'Work In Progress',
       shortDescription: '',
       longDescription: 'In summer of 2025 I got the opportunity to work as an Ink & Paint artist on the short film "La verdad te rechaza, Cornelius." directed by Khris Cembe and Marc Torices via Hampa Studio.',
       thumbnail: '/images/placeholder.svg', // For optimized images: import from assets and use .src
